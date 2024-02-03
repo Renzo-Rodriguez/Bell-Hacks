ef854a353f57ef57496a30b91885436cc378fb8d
Renzo Rodriguez <rr.renzorodriguez@gmail.com>
2024-02-03 10:40:24 -0800

    bheta bery good

---
+++ main/predata.py
@@ -0,0 +1,12 @@
+#need to
+#probably a csv
+import pandas as pan 
+import numpy as np 
+import os 
+import matplotlib as plt
+
+
+topology = Image.open("path-to-image").convert('RGBA')
+topology = np.array(topology)
+
+percipitation = Image.open
